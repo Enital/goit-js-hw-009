53333c835cf3cbf7c049560b7ba0b52484eb97df
Enital <117684114+Enital@users.noreply.github.com>
2023-04-10 11:27:49 +0300

    1!

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,23 +3,26 @@ const buttonStopEl = document.querySelector('button[data-stop]');
 const bodyEl = document.querySelector('body');
 let timerId = null;
 
+buttonStopEl.disabled = true;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
-function changeColor () {
-    const color = getRandomHexColor();
-    document.body.style.background = color;
-};
-
 
 buttonStartEl.addEventListener("click", () => {
+    buttonStartEl.disabled = true;
+    buttonStopEl.disabled = false;
     timerId = setInterval(() => {
         const color = getRandomHexColor();
         document.body.style.background = color;
     }, 1000)
 });
 
-buttonStopEl.addEventListener('click', () => {
+
+buttonStopEl.addEventListener('click', () =>{
     clearInterval(timerId);
+    buttonStopEl.disabled = true;
+    buttonStartEl.disabled = false;
+    intervalId = 1;
 });
