18c1c4fa613ea7f2311a6816c15afda11c2f5199
Enital <117684114+Enital@users.noreply.github.com>
2023-04-10 08:28:05 +0000

    Deploying to gh-pages from @ Enital/goit-js-hw-009@53333c835cf3cbf7c049560b7ba0b52484eb97df 🚀

--- 01-color-switcher.90428505.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");document.querySelector("body");let n=null;function o(){return`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(()=>{n=setInterval((()=>{const t=o();document.body.style.background=t}),1e3)})),e.addEventListener("click",(()=>{clearInterval(n)}));
-//# sourceMappingURL=01-color-switcher.90428505.js.map
---
+++ 01-color-switcher.bfb61871.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");document.querySelector("body");let d=null;e.disabled=!0,t.addEventListener("click",(()=>{t.disabled=!0,e.disabled=!1,d=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.background=t}),1e3)})),e.addEventListener("click",(()=>{clearInterval(d),e.disabled=!0,t.disabled=!1,intervalId=1}));
+//# sourceMappingURL=01-color-switcher.bfb61871.js.map
